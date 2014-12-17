bb6033cc45e2c8ca801c558777100fdca0958881
CLullaby <chrystel.juan@gmail.com>
2014-12-17 15:09:19 +0100

    add jars jsf, spl et tomcat

--- NowWhatToDo2/src/model/CompteModelBean.java
+++ NowWhatToDo2/src/model/CompteModelBean.java
@@ -1,5 +1,13 @@
 package model;
 
+import java.io.Serializable;
+
+import javax.annotation.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+
 public class CompteModelBean {
 
 }
