c157f55c286a87d71ea467e8d08178f46842e2f1
CLullaby <chrystel.juan@gmail.com>
2014-12-17 14:14:47 +0100

    daoFabrique et user cree

---
+++ NowWhatToDo2/src/dao/fabrique/DaoFabrique.java
@@ -0,0 +1,52 @@
+package dao.fabrique;
+
+import dao.instance.DaoUtilisateur;
+
+public class DaoFabrique {
+
+	private static volatile DaoFabrique instance = null;
+
+	private String DB_HOST = "localhost";
+	private String DB_PORT = "3306";
+	private String DB_NAME = "NowWhatToDo";
+	private String DB_USER = "NowWhatToDo";
+	private String DB_PWD = "NowWhatToDo";
+
+	private DaoFabrique() {
+		super();
+		try {
+			// Chargement du Driver, puis établissement de la connexion
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Méthode permettant de renvoyer une instance de la classe Singleton
+	 * 
+	 * @return Retourne l'instance du singleton.
+	 */
+	public final static DaoFabrique getInstance() {
+		// Le "Double-Checked Singleton"/"Singleton doublement vérifié" permet
+		// d'éviter un appel coûteux à synchronized,
+		// une fois que l'instanciation est faite.
+		if (DaoFabrique.instance == null) {
+			// Le mot-clé synchronized sur ce bloc empêche toute instanciation
+			// multiple même par différents "threads".
+			synchronized (DaoFabrique.class) {
+				if (DaoFabrique.instance == null) {
+					DaoFabrique.instance = new DaoFabrique();
+				}
+			}
+		}
+		return DaoFabrique.instance;
+	}
+
+	public DaoUtilisateur createUserDao() {
+		DaoUtilisateur userDao = new DaoUtilisateur(this.DB_HOST,this.DB_PORT,this.DB_NAME,this.DB_USER,this.DB_PWD);
+		return userDao;
+	}
+
+}
+
