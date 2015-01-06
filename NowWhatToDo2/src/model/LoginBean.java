741cb7c3dd56059079656b041e80599d0e717c2d
CLullaby <chrystel.juan@gmail.com>
2015-01-06 15:02:13 +0100

    login

---
+++ NowWhatToDo2/src/model/LoginBean.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class LoginBean implements Serializable{
+	private String identifiant;
+	private String motDePasse;
+	
+	public LoginBean() {
+	}
+
+	public String getIdentifiant() {
+		return identifiant;
+	}
+
+	public void setIdentifiant(String identifiant) {
+		this.identifiant = identifiant;
+	}
+
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+	
+}
