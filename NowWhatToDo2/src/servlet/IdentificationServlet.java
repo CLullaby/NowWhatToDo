4d153e4dfe67cdd63290d44ef842f8540a0f322d
Cissou241 <cyril.cabrol1@orange.fr>
2015-01-08 11:16:23 +0100

    Formulaire inscription  + envoie donnes a Servlet Ok

--- NowWhatToDo2/src/servlet/IdentificationServlet.java
+++ NowWhatToDo2/src/servlet/IdentificationServlet.java
@@ -1,28 +1,15 @@
 package servlet;
 
 import java.io.IOException;
-import java.util.Iterator;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-
-
-
-import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONValue;
-
-
-
-
-
-
 
 /**
  * Servlet implementation class IdentificationServlet
