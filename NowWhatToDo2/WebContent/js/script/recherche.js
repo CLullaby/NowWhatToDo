5b8628d8c2d61fdb979c97cfe21dc9de7411c373
SebMC12 <seb004@neuf.fr>
2015-01-15 11:27:25 +0100

    recherche

--- NowWhatToDo2/WebContent/js/script/recherche.js
+++ NowWhatToDo2/WebContent/js/script/recherche.js
@@ -1,7 +1,8 @@
 function lancerRecherche()
 {
+	var activiteTable = [];
 	var motCle = document.getElementById("inputMotCle").value;
-	if(motCle.match(/^[a-z0-9_-]{3,30}$/) && motCle != "")
+	if(motCle.match(/^[A-Za-z0-9_-]{3,30}$/) && motCle != "")
 	{
 		$.ajax({
 	        url: '../../Recherche',
@@ -15,7 +16,49 @@ function lancerRecherche()
 	        	//Pensez a dire si la liste est vide ici ou dans le JS
 	        	
 	        	//data.liste
-	
+	        	if(jQuery.isEmptyObject(data)){
+	        		
+	        		$('#messageVide').addClass('messageVide');
+	        		var span = document.createElement("p");
+	        		var text = document.createTextNode("Mot introuvable");
+	        		span.appendChild(text);	
+	            	//Permet de supprimer si un message est déja présent
+	            	var elm = document.getElementById('messageVide');
+	            	while (elm.firstChild) {
+	            		  elm.removeChild(elm.firstChild);
+	            	}
+	        		elm.appendChild(span);
+	        		
+	        	}
+	        	else{
+	        		
+	        		for(var i=0; i<data.length; i++){
+	    	        	
+		        		var activite = [];
+		        		var jsonActivite = data[i];
+		        		
+		        		//alert(jsonActivite);
+		        		
+		        		activite['nomActivite'] = jsonActivite.nomActivite;
+		        		activite['Description'] = jsonActivite.Description;
+		        		activite['nomLieu'] = jsonActivite.nomLieu;
+		        		activite['adresse'] = jsonActivite.adresse;
+		        		activite['ville'] = jsonActivite.ville;
+		        		activite['codePostal'] = jsonActivite.codePostal;
+		        		activite['siteWeb'] = jsonActivite.siteWeb;
+		        		activite['telephone'] = jsonActivite.telephone;
+		        		activite['email'] = jsonActivite.email;
+		        		activite['domaine'] = jsonActivite.domaine;
+		        		activite['lienPhoto'] = jsonActivite.lienPhoto;
+		        		activite['importance'] = jsonActivite.importance;
+		        		
+		        		activiteTable.push(activite);
+		        		
+	        		
+	        		}
+	        		
+	        		displayActivite(activiteTable,cheminImage,arraySizeImages);
+	        	}
 	        	
 	        	
 	        }
@@ -36,3 +79,39 @@ function lancerRecherche()
 	}
 
 }
+
+function displayActivite(activiteTable,cheminImage,arraySizeImages){
+	
+	$.each(activiteTable,function(key,value){	
+		
+		var stringNomActivite = value['nomActivite'];
+		var stringNomLieu = value['nomLieu'];
+		var stringAdresse = value['adresse'];
+		var stringCodePostal = value['codePostal'];
+		var stringVille = value['ville'];
+		var stringTelephone = value['telephone'];
+		var stringEmail = value['email'];
+		var stringTelephoneEtEmail = "<p>"+stringTelephone+" et "+stringEmail;
+		var stringSiteWeb = value['siteWeb'];
+		var stringDescription = value['Description'];
+		var stringLienPhoto = value['lienPhoto'];
+		
+		
+		var stringDivActivite = 
+									 
+											+"<p>"+stringNomActivite+"</p>"
+													+ "<ul class='list-group'>"
+												      	+"<li class='list-group-item'>"+stringNomLieu+stringAdresse+stringCodePostal+stringVille+"</li>"
+												      	+"<li class='list-group-item'>"+stringTelephoneEtEmail+"</li>"
+												      	+"<li class='list-group-item'>"+stringSiteWeb+"</li>"
+												    +"</ul>"
+												    +"<p>"+stringDescription+"</p>"
+												    +"</p>"stringLienPhoto+"</p>"
+												    
+								
+		;
+		$(".resultatRecherche").append(stringDivActivite);
+	});
+
+}
+	
