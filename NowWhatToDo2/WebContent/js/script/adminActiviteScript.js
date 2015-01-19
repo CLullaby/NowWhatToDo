b3480533d1fe5d8c9dc9dd9be85d6e024229bc34
Christophe Hu <hu_christophe@hotmail.fr>
2015-01-19 15:02:50 +0100

    Ajout du test de google maps

--- NowWhatToDo2/WebContent/js/script/adminActiviteScript.js
+++ NowWhatToDo2/WebContent/js/script/adminActiviteScript.js
@@ -1,10 +1,92 @@
-
+//JSON Array qui va contenir toutes les activités
+var jsonArray = new Array();
 
 //Au chargement de la page, affiche les activités sur la page d'administration
-function afficherActivites() {	
+function afficherActivites() {
+	
+	//Récupération du JSONArray contenant toutes les activités
 	getActivites();
 
+	// Récupération du JSON de chaque activité et de chaque
+	// champ dans chacune des activités
+	for (i = 0; i < jsonArray.length; ++i) {
+		var activiteJson = jsonArray[i];
+
+		var id = activiteJson.id;
+		var nomActivite = activiteJson.nomActivite;
+		var description = activiteJson.description;
+		var nomLieu = activiteJson.nomLieu;
+		var adresse = activiteJson.adresse;
+		var ville = activiteJson.ville;
+		var codePostal = activiteJson.codePostal;
+		var siteWeb = activiteJson.siteWeb;
+		var telephone = activiteJson.telephone;
+		var email = activiteJson.email;
+		var domaine = activiteJson.domaine;
+		var lienPhoto = activiteJson.lienPhoto;
+		var importance = activiteJson.importance;
+								
+		// Récupération du tableau dans la page HTML
+		var tableauHtml = document
+				.getElementById("tableauActivites");
+
+		// Ajout d'une ligne
+		tableauHtml.innerHTML += "<tr id=ligne" + id + ">";
+		var ligne = document.getElementById("ligne" + id);
+
+		// R�cup�ration et �criture de chaque champ dans le
+		// tableau HTML
+		var colonneDomaine = ligne.insertCell(-1);// on a une
+		// ajout�
+		// une
+		// cellule
+		colonneDomaine.innerHTML += "<td>" + domaine + "</td>";
+
+		var colonneActivite = ligne.insertCell(1);// on ajoute
+		// la
+		// seconde
+		// cellule
+		colonneActivite.innerHTML += "<td>" + nomActivite
+				+ "</td>";
+
+		var colonneLieu = ligne.insertCell(2);
+		colonneLieu.innerHTML += "<td>" + nomLieu + "</td>";
+
+		var colonneAdresse = ligne.insertCell(3);
+		colonneAdresse.innerHTML += "<td>" + adresse + "</td>";
+
+		var colonneVille = ligne.insertCell(4);
+		colonneVille.innerHTML += "<td>" + ville + "</td>";
+
+		var colonneCodePostal = ligne.insertCell(5);
+		colonneCodePostal.innerHTML += "<td>" + codePostal
+				+ "</td>";
+
+		var colonneSiteWeb = ligne.insertCell(6);
+		colonneSiteWeb.innerHTML += "<td>" + siteWeb + "</td>";
+
+		var colonneTelephone = ligne.insertCell(7);
+		colonneTelephone.innerHTML += "<td>" + telephone
+				+ "</td>";
+
+		var colonneImportance = ligne.insertCell(8);
+		colonneImportance.innerHTML += "<td>" + importance
+				+ "</td>";
+
+		var colonneBoutons = ligne.insertCell(9);
+		colonneBoutons.innerHTML += "<td>"
+				+ "<button type=\"submit\" class =\"btn btn-danger btn-sm\" onclick=\"supprimerActivite("+ id +")\">"
+				+ "<span class=\"glyphicon glyphicon-remove\">"
+				+ "</span>"
+				+ "</button>"
+				+ "<button type=\"submit\" class=\"btn btn-success btn-sm\" onclick=\"modifierActivite("+ id +")\">"
+				+ "<span class=\"glyphicon glyphicon-pencil\">"
+				+ "</span>" + "</button>" + "</td>";
+
+		// Fermeture de la balise ligne
+		tableauHtml.innerHTML += "</tr>";
 }
+	}
 
 // Récupère les Activités stockées dans la base de données
 function getActivites() {
@@ -15,93 +97,13 @@ function getActivites() {
 				type : 'GET',
 				dataType : 'json',
 				success : function(data) {
+					//Vérification de la connexion à un compte admin
 					if(data.connecte == "oui")
 					{			
-						var jsonArray = new Array();
-						jsonArray = data.listeActivites;
-						// Récupération du JSON de chaque activité et de chaque
-						// champ dans chacune des activités
-						for (i = 0; i < jsonArray.length; ++i) {
-							var activiteJson = jsonArray[i];
-	
-							var id = activiteJson.id;
-							var nomActivite = activiteJson.nomActivite;
-							var description = activiteJson.description;
-							var nomLieu = activiteJson.nomLieu;
-							var adresse = activiteJson.adresse;
-							var ville = activiteJson.ville;
-							var codePostal = activiteJson.codePostal;
-							var siteWeb = activiteJson.siteWeb;
-							var telephone = activiteJson.telephone;
-							var email = activiteJson.email;
-							var domaine = activiteJson.domaine;
-							var lienPhoto = activiteJson.lienPhoto;
-							var importance = activiteJson.importance;
-													
-							// Récupération du tableau dans la page HTML
-							var tableauHtml = document
-									.getElementById("tableauActivites");
-	
-							// Ajout d'une ligne
-							tableauHtml.innerHTML += "<tr id=ligne" + id + ">";
-							var ligne = document.getElementById("ligne" + id);
-	
-							// R�cup�ration et �criture de chaque champ dans le
-							// tableau HTML
-							var colonneDomaine = ligne.insertCell(-1);// on a une
-							// ajout�
-							// une
-							// cellule
-							colonneDomaine.innerHTML += "<td>" + domaine + "</td>";
-	
-							var colonneActivite = ligne.insertCell(1);// on ajoute
-							// la
-							// seconde
-							// cellule
-							colonneActivite.innerHTML += "<td>" + nomActivite
-									+ "</td>";
-	
-							var colonneLieu = ligne.insertCell(2);
-							colonneLieu.innerHTML += "<td>" + nomLieu + "</td>";
-	
-							var colonneAdresse = ligne.insertCell(3);
-							colonneAdresse.innerHTML += "<td>" + adresse + "</td>";
-	
-							var colonneVille = ligne.insertCell(4);
-							colonneVille.innerHTML += "<td>" + ville + "</td>";
-	
-							var colonneCodePostal = ligne.insertCell(5);
-							colonneCodePostal.innerHTML += "<td>" + codePostal
-									+ "</td>";
-	
-							var colonneSiteWeb = ligne.insertCell(6);
-							colonneSiteWeb.innerHTML += "<td>" + siteWeb + "</td>";
-	
-							var colonneTelephone = ligne.insertCell(7);
-							colonneTelephone.innerHTML += "<td>" + telephone
-									+ "</td>";
-	
-							var colonneImportance = ligne.insertCell(8);
-							colonneImportance.innerHTML += "<td>" + importance
-									+ "</td>";
-	
-							var colonneBoutons = ligne.insertCell(9);
-							colonneBoutons.innerHTML += "<td>"
-									+ "<button type=\"submit\" class =\"btn btn-danger btn-sm\" onclick=\"supprimerActivite("+ id +")\">"
-									+ "<span class=\"glyphicon glyphicon-remove\">"
-									+ "</span>"
-									+ "</button>"
-									+ "<button type=\"submit\" class=\"btn btn-success btn-sm\" onclick=\"modifierActivite("+ id +")\">"
-									+ "<span class=\"glyphicon glyphicon-pencil\">"
-									+ "</span>" + "</button>" + "</td>";
-	
-							// Fermeture de la balise ligne
-							tableauHtml.innerHTML += "</tr>";
-						}
-						
-//						//Trigger pour avertir de l'ajout dynamique de lignes dans le tableau
-//						$('.tablesorter').trigger('update');
+						//var jsonArray = new Array();
+						jsonArray = data.listeActivites;		
 					}
+					//Si pas de connexion a un compte admin redirection vers la page de login
 					else
 					{
 						location.href = "loginAdmin.html";
@@ -111,7 +113,7 @@ function getActivites() {
 					alert("Les activités n'ont pas pu être chargées correctement.");
 				}
 			});
-//	triTableau();
+
 }
 
 function supprimerActivite(idActivite) {
@@ -147,8 +149,3 @@ function modifierActivite(idActivite) {
 	location.href="creerActivite.html?id="+idActivite;
 }
 
-
-////Fonction qui permet le tri du tableau par paramètre
-//function triTableau(){
-//	$("#tableauActivites").tablesorter();
-//}
